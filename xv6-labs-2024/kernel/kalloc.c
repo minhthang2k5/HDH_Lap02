fbfe0cebb26825d95515432cb1ba81d6b1014996
huuthang-0355 <vohuuthang3110@gmail.com>
2025-11-16 22:59:30 +0700

    freemen and getnprocs

--- xv6-labs-2024/kernel/kalloc.c
+++ xv6-labs-2024/kernel/kalloc.c
@@ -80,3 +80,18 @@ kalloc(void)
     memset((char*)r, 5, PGSIZE); // fill with junk
   return (void*)r;
 }
+
+// Đếm số page trống -> nhân vs kích thước trang -> số byte trống
+uint64
+freemem(void) {
+  struct run* r;
+  uint64 count = 0;
+
+  r = kmem.freelist;
+  while(r) {
+    count++;
+    r = r->next;
+  }
+
+  return count * PGSIZE;
+}
--- xv6-labs-2024/kernel/proc.c
+++ xv6-labs-2024/kernel/proc.c
@@ -693,3 +693,21 @@ procdump(void)
     printf("\n");
   }
 }
+
+uint64
+getnproc(void) {
+  struct proc* p;
+  uint64 count = 0;
+
+  for(p = proc; p < &proc[NPROC]; p++) {
+    
+    acquire(&p->lock);
+    if(p->state != UNUSED) {
+      count++;
+    }
+
+    release(&p->lock);
+  }
+
+  return count;
+}
